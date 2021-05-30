914870abc85725dc57996a34911b2402794ea353
Glitch (glitch-hello-node) <none>
2021-05-30 00:10:49 +0000

    🌿🚪 Checkpoint ./commands/prefix.js:27161867/5650

---
+++ commands/prefix.js
@@ -0,0 +1,45 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  "database",
+  process.env.DBUSERNAME,
+  process.env.DBPASSWORD,
+  {
+    host: "localhost",
+    dialect: "sqlite",
+    logging: false,
+    storage: "database.sqlite"
+  }
+);
+
+const Servers = sequelize.define("Servers", {
+  serverId: {
+    type: Sequelize.STRING(25),
+    primarykey: true,
+    allowNull: false,
+    unique: true
+  },
+  channelId: {
+    type: Sequelize.STRING(25),
+    allowNull: false
+  },
+  prefix: {
+    type: Sequelize.STRING(5),
+    allowNull: false,
+    defaultValue: "!"
+  }
+});
+
+module.exports = {
+  name: "Prefix",
+  pattern: /prefix/i,
+  execute: async function(msg, tokens) {
+    if (!tokens[0])
+      return msg.reply("Please specify a symbol to replace the current prefix");
+    await Servers.sync();
+    let record = Servers.findOne({ where: { serverId: msg.guild.id } });
+    if (!record) return msg.reply("Error: no entry found in server list");
+    let prefix = tokens[0].charAt(0);
+    record.prefix = prefix;
+    
+  }
+};
