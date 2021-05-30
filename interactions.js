721280876c2b39d13809ef6b7bfa494cd1cd40f6
Glitch (glitch-hello-node) <none>
2021-05-30 19:59:43 +0000

    🌒🎥 Checkpoint ./interactions.js:27161867/2560

---
+++ interactions.js
@@ -0,0 +1,86 @@
+let interactions = [];
+const commandFiles = require("./util/getCommands.js");
+
+for (const file of commandFiles) {
+  let command = require(`./commands/${file}`);
+  interactions.push(command);
+}
+
+const Sequelize = require("sequelize");
+const { Op } = require("sequelize");
+const sequelize = new Sequelize(
+  "database",
+  process.env.DBUSERNAME,
+  process.env.DBPASSWORD,
+  {
+    host: "localhost",
+    dialect: "sqlite",
+    logging: false,
+    storage: "database.sqlite"
+  }
+);
+
+const Servers = sequelize.define("Servers", {
+  serverId: {
+    type: Sequelize.STRING(25),
+    primarykey: true,
+    allowNull: false,
+    unique: true
+  },
+  channelId: {
+    type: Sequelize.STRING(25),
+    allowNull: false
+  },
+  prefix: {
+    type: Sequelize.STRING(5),
+    allowNull: false,
+    defaultValue: "!"
+  }
+});
+
+module.exports = async function(interaction, Client) {
+  await Servers.sync();
+  let record = await Servers.findOne({
+    where: {
+      [Op.and]: [{ serverId: interaction.guild_id }, { channelId: interaction.channel_id }]
+    },
+    attributes: ["prefix"]
+  });
+  //console.log(record);
+  
+
+  if (record) {
+    //return msg.channel.send("pong");
+    //console.log("command identified");
+
+    let tokens = interaction.data;
+
+    // if (!commands.has(command)) return;
+
+    // //console.log("commands has command ");
+
+    for (const c of interactions) {
+      //console.log(c);
+
+      if (c.pattern.test(interaction.data.nam)) {
+        try {
+          c.execute(interaction, Client);
+
+          //console.log("command executed");
+        } catch (error) {
+          console.log(error);
+
+          //msg.reply("There was an error executing that command :(");
+        } finally {
+          break;
+        }
+      }
+    }
+  } else if (interaction.data.name === "bind") {
+    let created = await Servers.create({
+      serverId: interaction.guild_id,
+      channelId: interaction.channel_id 
+    });
+    msg.channel.send("Phantom's Abyss Catacombs bound to this channel");
+  }
+};
