bf5607396f9edc0ae905383d0285870f3601e156
Glitch (glitch-hello-node) <none>
2021-06-02 22:34:44 +0000

    🕷🍮 Checkpoint ./interactions/check.js:27161867/1093

--- interactions/check.js
+++ interactions/check.js
@@ -1,5 +1,19 @@
 module.exports = {
   name: "check",
   pattern: /\bcheck\b/i,
-  execute: function(interaction, Client) {}
+  execute: function(interaction, Client) {
+    console.log(interaction.data.options);
+  },
+  addInteraction: {
+    name: "check",
+    description: "Check the status of a player.",
+    options: [
+      {
+        type: 9,
+        name: "player",
+        description: "Player to check. If left blank, will check you",
+        required: false
+      }
+    ]
+  }
 };
