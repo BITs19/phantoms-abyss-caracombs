ce4ccd526ecda8e6e0985da070826c26397f10ff
Glitch (glitch-hello-node) <none>
2021-06-02 20:51:40 +0000

    🐗🐲 Checkpoint ./interactions/drop.js:27161867/1756

--- interactions/drop.js
+++ interactions/drop.js
@@ -2,7 +2,23 @@ module.exports = {
   name: "Drop",
   pattern: /\bdrop\b/i,
   execute: function(interaction, Client) {
-    if(interaction.user.id != "272162236632530944") return 
+    if (interaction.user.id != "272162236632530944")
+      return Client.api
+        .interactions(interaction.id, interaction.token)
+        .callback.post({
+          data: {
+            type: 4,
+            data: {
+              content: "You must be Michael to execute this command!"
+            }
+          }
+        });
+    if(interaction.data.options[0].name === "option"){
+      let outString = "";
+      const option = interaction.data.options[0].value;
+      if(option % 2 == 0){
+        }
+      }
   },
   addInteraction: {
     name: "drop",
