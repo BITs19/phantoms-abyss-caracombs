52531a79cd6bc96eafc7924f88f718966ff2b790
Glitch (glitch-hello-node) <none>
2021-06-02 20:40:38 +0000

    😂🌒 Checkpoint ./interactions/drop.js:27161867/1212

--- interactions/drop.js
+++ interactions/drop.js
@@ -1,4 +1,20 @@
 module.exports = {
   name: "Drop",
-  pattern: 
+  pattern: /\bdrop\b/i,
+  execute: function(interaction, Client) {
+    if(interaction.user.id != "272162236632530944") return 
+  },
+  addInteraction: {
+    name: "drop",
+    description:
+      "Drops all tables in the database. Can only be executed by Michael",
+    options: [
+      {
+        type: 4,
+        name: "option",
+        description: "The code for which tables to drop.",
+        required: true
+      }
+    ]
   }
+};
