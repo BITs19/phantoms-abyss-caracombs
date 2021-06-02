43cc5dd5625671691ae76c07e8cceba116591b4f
Glitch (glitch-hello-node) <none>
2021-06-02 19:44:17 +0000

    🚄🏀 Checkpoint ./interactions/join.js:27161867/406

--- interactions/join.js
+++ interactions/join.js
@@ -16,7 +16,8 @@ const Players = sequelize.define("players", {
   id: {
     type: Sequelize.STRING(25),
     primaryKey: true,
-    allowNull: false
+    allowNull: false,
+    unique: true
   },
   carrying: {
     type: Sequelize.INTEGER(1),
@@ -25,6 +26,24 @@ const Players = sequelize.define("players", {
   score: {
     type: Sequelize.INTEGER,
     defaultValue: 0
+  },
+});
+
+const Servers = sequelize.define("Servers", {
+  serverId: {
+    type: Sequelize.STRING(25),
+    primarykey: true,
+    allowNull: false,
+    unique: true
+  },
+  channelId: {
+    type: Sequelize.STRING(25),
+    allowNull: false
+  },
+  prefix: {
+    type: Sequelize.STRING(5),
+    allowNull: false,
+    defaultValue: "!"
   }
 });
 
@@ -32,7 +51,7 @@ module.exports = {
   name: "join",
   pattern: /join/i,
   execute(interaction, Client) {
-    
+    Players.create({id: interaction.member.user.id})
     Client.api.interactions(interaction.id, interaction.token).callback.post({
       data: {
         type: 4,
