834a58be982a9605c90d97981185c6fb76040211
Glitch (glitch-hello-node) <none>
2021-06-03 01:07:03 +0000

    ⚡️📉 Checkpoint ./interactions/ping.js:27161867/4

--- interactions/ping.js
+++ interactions/ping.js
@@ -2,12 +2,12 @@ module.exports = {
   name: "ping",
   pattern: /ping/i,
   execute: async function(interaction, Client) {
-    console.log(interaction.member.user.id);
+    /*console.log(interaction.member.user.id);
     console.log(
       (await (await Client.guilds.fetch(interaction.guild_id)).members.fetch(
         interaction.member.user.id
       )).id
-    );
+    );*/
     Client.api.interactions(interaction.id, interaction.token).callback.post({
       data: {
         type: 4,
