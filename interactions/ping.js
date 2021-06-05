22ca3e7a01def1fba12d49f2e5a36e153ce65cce
Glitch (glitch-hello-node) <none>
2021-06-05 21:44:42 +0000

    ⏰🏃 Checkpoint ./interactions/ping.js:27161867/4166

--- interactions/ping.js
+++ interactions/ping.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 module.exports = {
   name: "ping",
   pattern: /ping/i,
@@ -8,14 +10,30 @@ module.exports = {
         interaction.member.user.id
       )).id
     );*/
-    Client.api.interactions(interaction.id, interaction.token).callback.post({
-      data: {
-        type: 4,
+    try {
+      const jsonString = fs.readFileSync(
+        "https://cdn.glitch.com/778ebb69-2cd4-4967-9ee6-0f93ce3868f9%2Fpacman.json?v=1622834537975"
+      );
+      const json = JSON.parse(jsonString);
+      Client.api.interactions(interaction.id, interaction.token).callback.post({
         data: {
-          content: "Pong!"
+          type: 4,
+          data: {
+            content: "Pong!"
+          }
         }
-      }
-    });
+      });
+    } catch (err) {
+      console.log(err);
+      Client.api.interactions(interaction.id, interaction.token).callback.post({
+        data: {
+          type: 4,
+          data: {
+            content: "There was an error. It has been logged!"
+          }
+        }
+      });
+    }
   },
   addInteraction: {
     name: "ping",
