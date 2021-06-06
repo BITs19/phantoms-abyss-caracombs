74bf6b8538f5d9b076e531c9ebb55b0505a117ed
Glitch (glitch-hello-node) <none>
2021-06-06 01:14:50 +0000

    🙈🛢 Checkpoint ./interactions/room.js:27161867/3577

--- interactions/room.js
+++ interactions/room.js
@@ -1,5 +1,28 @@
 const Maze = require("../util/getMazeTable.js");
+const Players = require("../util/getPlayerTable.js");
 
 module.exports = {
-  
+  name: "room",
+  pattern: /\broom\b/i,
+  execute: async function(interaction, Client) {
+    const player = await Players.findOne({
+      where: { id: interaction.member.user.id, serversId: interaction.guild_id }
+    });
+    if (!player)
+      return Client.api
+        .interactions(interaction.id, interaction.token)
+        .callback.post({
+          data: {
+            type: 4,
+            data: {
+              content: "You don't appear to be a player in this server!"
+            }
+          }
+        });
+    const room = await Maze.findOne({where: {id: player.roomId}})
+  },
+  addInteraction: {
+    name: "room",
+    description: "Look around the room you're in."
   }
+};
