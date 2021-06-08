dcd9cb3383ce8fd2177e5de1dd50562df91b18de
Glitch (glitch-hello-node) <none>
2021-06-08 20:03:00 +0000

    ✍️🔖 Checkpoint ./interactions/eat.js:27161867/5441

--- interactions/eat.js
+++ interactions/eat.js
@@ -2,6 +2,7 @@ const replyInteraction = require("../util/replyInteraction.js");
 const Maze = require("../util/getMazeTable.js");
 const Players = require("../util/getPlayerTable.js");
 const PickedPellets = require("../util/getPickedPellets.js");
+const { Op } = require("sequelize");
 
 module.exports = {
   name: "eat",
@@ -21,7 +22,24 @@ module.exports = {
     PickedPellets.sync();
     Maze.sync();
 
+    let playersInRoom = null;
+    if (player.energized) {
+      playersInRoom = await Players.findAll({
+        where: { roomId: player.roomId, id: { [Op.not]: player.id } }
+      });
+    }
+
     const room = await Maze.findOne({ where: { id: player.roomId } });
+
+    const pelletPicked = PickedPellets.count({
+      where: { roomId: room.id, serverId: interaction.guild_id }
+    });
+    
+    if(pelletPicked == 0){
+      if(room.pellet){
+        
+      }
+    }
     replyInteraction(Client, interaction, "foo");
   },
   addInteraction: {
--- util/holdables.js
+++
@@ -1,1 +0,0 @@
-module.exports = ["Pellet", "Energizer", "Ghost"];
