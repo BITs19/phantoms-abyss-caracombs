515f2ae900d934fe3e4313d2fa369d436642fb3e
Glitch (glitch-hello-node) <none>
2021-06-10 20:52:11 +0000

    🐝🔔 Checkpoint ./util/getGhostsTable.js:27161867/344

---
+++ util/getGhostsTable.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+//const { Op } = require("sequelize");
+const sequelize = new Sequelize(
+  "database",
+  process.env.DBUSERNAME,
+  process.env.DBPASSWORD,
+  {
+    host: "localhost",
+    dialect: "sqlite",
+    logging: false,
+    storage: "database.sqlite"
+  }
+);
+
+const Ghosts = sequelize.define("ghosts",{
+ 
+})
