c8d18d48e2b3f1a1c75694888684f40e2efccb51
Glitch (glitch-hello-node) <none>
2021-06-11 00:50:18 +0000

    💶🚋 Checkpoint ./util/updateGhosts.js:27161867/3717

--- util/updateGhosts.js
+++ util/updateGhosts.js
@@ -1,9 +1,20 @@
 const Ghosts = require("./getGhostsTable.js");
 const PickedPellets = require("./getPickedPellets.js");
+const Players = require("./getPlayerTable.js");
 
 module.exports = async function(userId, serverId) {
   Ghosts.sync();
+  Players.sync();
+  const player = await Players.findOne({
+    where: { id: userId, serverId: serverId }
+  });
   let serverPlayerGhosts = await Ghosts.findAll({
     where: { playerId: userId, serverId: serverId }
   });
+  for (let ghost of serverPlayerGhosts) {
+    let targetId;
+    switch(ghost.subId){
+        }
+    
+  }
 };
